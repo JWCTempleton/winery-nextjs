855406a34592c51e595937a1e690a9094dd9bf25
Jacob Templeton <jwctempleton@gmail.com>
2022-08-20 15:42:40 -0700

    Add basic home page

--- winery-nextjs/pages/index.js
+++ winery-nextjs/pages/index.js
@@ -1,6 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
+import Home from "../components/Home";
 
-export default function Home() {
-  return <div>Test</div>;
+export default function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
