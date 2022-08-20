b1bf26c33aace2ed2c876de86264e2917f7f51f5
Jacob Templeton <jwctempleton@gmail.com>
2022-08-20 16:29:57 -0700

    Add logo to homepage, color styling

--- winery-nextjs/components/Home.js
+++ winery-nextjs/components/Home.js
@@ -1,6 +1,22 @@
+import Image from "next/image";
+import ofvLogo from "../images/ofv_logo_white.png";
+
 export default function Home() {
+  const LogoImage = (props) => {
+    return (
+      <Image
+        className="home-logo"
+        src={ofvLogo}
+        alt="ofv logo"
+        height="150px"
+        width="150px"
+      />
+    );
+  };
+
   return (
     <div className="home">
+      <LogoImage />
       <h1 className="home-title">Olney Family Vineyards</h1>
     </div>
   );
