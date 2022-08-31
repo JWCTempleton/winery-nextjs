c7eeacf0eba81ce58c4dbe1efe98bc759c33bfdd
Jacob Templeton <jwctempleton@gmail.com>
2022-08-31 16:34:05 -0700

    Add paragraph text and paragraph styling

--- winery-nextjs/pages/about.js
+++ winery-nextjs/pages/about.js
@@ -4,8 +4,27 @@ export default function About() {
       <div className="about">
         <div className="about-image">image</div>
         <div className="about-info">
-          <p>first para</p>
-          <p>Second para</p>
+          <h1 className="para-title">Our Beginning</h1>
+          <p className="para-1">
+            Starting in 1996, We developed our Napa property and began
+            culitivating Cabernet Sauvignon Wine Grapes, to produce ultra
+            premium wines. Many years before our ownership, our property was a
+            plum orchard that was left as grassland. We nurtured the soil to
+            prepare it for wine grapes. The land had never grown wine grapes,
+            which made it ideal for our vineyard. With the soil prepared we then
+            choose and planted a little known grape vine from New York
+            State&apos;s Hermann J. Wiemer&apos;s Vineyard, now locally known as
+            the &quot;Wiemer Clone&quot;. The Wiemer grape vine grown here
+            produces some of the highest quality fruit in Napa Valley.
+          </p>
+          <br />
+          <p className="para-2">
+            In 2009 we graphed some newly planted rootstock to a special
+            Chardonnay bud with lineage dating back to the exceptional and
+            historic Wente Chardonnay clone. We released our first Chardonnay
+            wine in 2014 and continue to release the best chardonnays Napa
+            produces.
+          </p>
         </div>
       </div>
     </div>
