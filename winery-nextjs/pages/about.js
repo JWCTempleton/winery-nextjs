ea756ad76155a5c65d82751a30b365c165f336bb
Jacob Templeton <jwctempleton@gmail.com>
2022-08-31 15:27:10 -0700

    Add initial styling for About page

--- winery-nextjs/pages/about.js
+++ winery-nextjs/pages/about.js
@@ -1,3 +1,13 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="container">
+      <div className="about">
+        <div className="about-image">image</div>
+        <div className="about-info">
+          <p>first para</p>
+          <p>Second para</p>
+        </div>
+      </div>
+    </div>
+  );
 }
