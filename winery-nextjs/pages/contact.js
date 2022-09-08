13fe791bd7fdd2309e45cbe2b03233d8b4b044f7
Jacob Templeton <jwctempleton@gmail.com>
2022-09-07 19:56:05 -0700

    Add contact us page title

--- winery-nextjs/pages/contact.js
+++ winery-nextjs/pages/contact.js
@@ -1,3 +1,10 @@
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="container">
+      <div className="contact-title">
+        <h1 className="contact-title--main">Olney Family Vineyard</h1>
+        <h2 className="contact-title--sub">Winery and Cave</h2>
+      </div>
+    </div>
+  );
 }
