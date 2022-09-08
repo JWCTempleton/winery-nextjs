4bb3d6be84b531428c619f5575a2f41e56359b18
Jacob Templeton <jwctempleton@gmail.com>
2022-09-08 13:49:43 -0700

    Add starting contact info, starting contact info forms

--- winery-nextjs/pages/contact.js
+++ winery-nextjs/pages/contact.js
@@ -5,6 +5,19 @@ export default function Contact() {
         <h1 className="contact-title--main">Olney Family Vineyard</h1>
         <h2 className="contact-title--sub">Winery and Cave</h2>
       </div>
+      <div className="contact-section">
+        <div className="contact-section--info">Olney Family Vineyards</div>
+        <div className="contact-section--form">
+          <form>
+            <input
+              type="text"
+              id="fname"
+              name="firstname"
+              placeholder="Your name.."
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
