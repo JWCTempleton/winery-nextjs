5b9cab58027743597f098a45ae4ca2d8c42d7ecf
Jacob Templeton <jwctempleton@gmail.com>
2022-09-08 17:16:19 -0700

    Add visit page content and styling

--- winery-nextjs/pages/visit.js
+++ winery-nextjs/pages/visit.js
@@ -1,3 +1,34 @@
 export default function VisitUs() {
-  return <div>Visit</div>;
+  return (
+    <div className="container">
+      <div className="visit">
+        <div className="visit--header">
+          <h1>Tours, Tasting and Events</h1>
+        </div>
+        <div className="visit--info">
+          <p>
+            Vineyard, Cave and Winery tours are available by appointment. The
+            vineyard tour depends on the weather. The cave winery is protected
+            from the weather making it always available for tasting and tour.
+          </p>
+          <p>
+            The fall is the best time if you want to taste fruit in the
+            vineyard, see harvest activities and the winemaking and fermentation
+            process. The rest of the year is at a slower pace. We try to offer a
+            barrel tasting of wines at various stages of the wine making process
+            when available.
+          </p>
+          <p>
+            We offer for tasting of our released Cabernet Sauvignon and
+            Chardonnay wines. The tasting fee for the tour and one vintage is
+            $25.00 per person.
+          </p>
+          <p>
+            Our Cave is also available for special events. Contact us for more
+            information.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
